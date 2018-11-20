2755d298c896b56476b47ec48dc605621a3c860f
Dletta <jcduschletta@gmail.com>
2018-11-19 21:35:38 -0800

    implement a ruleStore, rule, concepts and relations objects to start building

--- gOps.js
+++ gOps.js
@@ -7,15 +7,15 @@
  Taking a master graph w, graph v (e.g. a query) is a projection of w when
  v can be derived from w by applying n>=0 detach ops or n>=0 restrict ops
  */
-
+/*
  for(each concept relation) {
   apply detachment on relation that is not in v
  }
  return subset of w that should contain v;
 
- /* In Gun a projection is achieved by doing gun.get('edges').map().once
+ In Gun a projection is achieved by doing gun.get('edges').map().once
   and only keeping the edges that are found in the query v
-  */
+
 
   var projection = (query) {
     this.queryG = query;
@@ -47,18 +47,112 @@
         (which means the common concept of the 3 graphs are joined)
     }
   }
-
-function concept (label, value, quant, uuid) {
+*/
+function Concept (label, value, quant, uuid) {
+  this.uuid = uuid;
+  this.label = label;
+  this.value = value;
+  this.quant = quant;
+  /*
  if(value === undefined && quant === undefined){
-   concept indefinite
+   this.value = undefined;
+   this.quant = undefined;
+   // concept indefinite -> selector
  } else if (value) {
-   concept constant //one value for one person
+   this.value = value;
+   this.quant = undefined;
+   // concept constant -> a defined thing
    function permissible to check  if value is permissible on a given sort label
  } else if (quant) {
-   concept quantified // one of four value A E E1 E-set
+   this.value = undefined;
+   this.quant = quant;
+   // concept quantified ->  one of four value A E1 E-set
  }
+ */
 }
 
-function relation (label, uuid){
-
+function Relation (label, uuid, source, target){
+  this.uuid = uuid;
+  this.label = label;
+  this.source = source;
+  this.target = target;
 }
+
+
+/* Local Graph Functions
+ * To build a graph for queries
+ * To build a graph to perform graph operations on
+ */
+
+ function Rule () {
+   this.concept = [],
+   this.relation = [],
+   this.addC = function (item) {
+     item.uuid = uuidv4();
+     this.concept.push(item);
+     return item.uuid;
+   },
+   this.addR = function (item) {
+     item.uuid = uuidv4();
+     this.relation.push(item);
+     return item.uuid;
+   },
+   this.find = function (uuid) {
+     var i = 0;
+     var lC = this.concept.length;
+     for(i;i<lC;i++){
+       if(this.concept[i].uuid === uuid) {
+         return this.concept[i];
+       }
+     }
+     var y = 0;
+     var lR = this.relation.length;
+     for(y;y<lR;y++){
+       if(this.relation[y].uuid === uuid) {
+         return this.relation[y];
+       }
+     }
+
+     console.log('Error: uuid not found');
+   }
+ }
+
+ function ruleStore (domainLabel) {
+   this.label = domainLabel;
+   this.rules = [];
+   this.find = function (label) {
+     var i = 0;
+     var l = this.rules.length;
+     for(i;i<l;i++){
+       if(this.rules[i].label === label) {
+         return this.rules[i];
+       }
+     }
+     console.log('Error: no relation found');
+   }
+ }
+
+ function uuidv4 () {
+   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxx'.replace(/[xy]/g, function(c) {
+     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+     return v.toString(16);
+   })
+ }
+
+ var rStore = new ruleStore ('app');
+ var add = new Rule();
+ var number1 = new Concept('Number', undefined, 'A', uuidv4());
+ add.addC(number1);
+ var number2 = new Concept('Number', undefined, 'A', uuidv4());
+ add.addC(number2);
+ var sum = new Concept('Sum', undefined, undefined, uuidv4());
+ add.addC(sum);
+ var numberE = new Concept('Number',undefined,'E1', uuidv4());
+ add.addC(numberE);
+ var arg1 = new Relation('arg1', uuidv4(), number1.uuid, sum.uuid);
+ add.addR(arg1);
+ var arg2 = new Relation('arg2', uuidv4(), number2.uuid, sum.uuid);
+ add.addR(arg2);
+ var res = new Relation('res', uuidv4(), sum.uuid, numberE.uuid);
+ add.addR(res);
+ rStore.rules.push(add);
