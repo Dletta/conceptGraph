4be4056c2a83564bad28682ef920385fa941fbb7
Dletta <jcduschletta@gmail.com>
2018-11-23 22:28:16 -0800

    more info

--- queryAlgo.js
+++ queryAlgo.js
@@ -4,7 +4,7 @@ Theory straight from John F. Sowa's Paper titled
  All credit for the Theory of Conceptual Graphs goes to Sowa amongst many
  other great minds before him, who believe in a better, more human-like
  computer interaction. We truly stand on the shoulders of giants.
- 
+
 
  Answer Graph Criterias to check for
 
@@ -35,23 +35,45 @@ Theory straight from John F. Sowa's Paper titled
   Algorithm C
 
   w: = q;
-  while ( there is a preferred join j with w)
+  while ( there is a preferred join j with w)  // seems to imply we have a list of joins
     do begin
-      w: = result of performing j with w;
+      w: = result of performing j with w; //need function to do a join, also a score function
       while (there is a source conept a in w &&
             a does not have a value &&
             a does not have a question mark &&
             the target of a has a question mark)
-        do place a question mark on a;
+        do place a question mark on a; //propagation
       while (there is a target concept b in w &&
              b has a question mark &&
              all sources of b have values)
-        do get a value for b from it's access procedure (function);
+        do get a value for b from it's access procedure (function); // reasoning
      if (there are no question marks left in w and all of q has been covered
          by some join)
          then begin:
-          print answer;
+          print answer; // result
           stop
           end
     end.
+
+So as preparation of running a query through algorithm c we need to find
+possible joins and rank them per explanation in paper.
+
+ Given a set of possible joins s1 ... sn, score each graph by
+ - One point for each concept in j that is the same as in w
+ - One point per concept that is in q (original query)
+ - If a question mark in w is covered by a target in j another point
+ - If a question mark is covering a source concept subtract one point
+ - If a concept in w, with a value is covered by source in a function link
+   add a point, if it is covered by a target, reject j.
+ - each concept or relation in q that has not been covered, if j covers it
+   add a point each
+
+Also need to consider a function that can return true if something is  a subsort
+of a specific concept type.
+
+Also indices for each type of concept or target or source links, if performance
+issues occur for searching the schematic universe.
+
+prompting points (might not need this) are whenever it cannot find a join
+but a question mark remains on a quant('A')
 */
