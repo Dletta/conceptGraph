f1be7b6c03f8675923f2d50365ce794cd4119d84
Jacob Friedman <jacob@solvent.ca>
2018-12-07 12:18:51 -0500

    cleanup

--- gOps.js
+++ gOps.js
@@ -1,107 +1,11 @@
-/*
-* The term that makes up the universe
-* @constructor
-* @param {string} label - a string to represent the thing
-* @param {string} type - axiom, concept, relation, function?
-* @param {uuid} uuid - unique identifier
-* @param {object} arcs - objectArray of arcs in order as defined in ontology (relation)
-* @param {string, int, undefined} value - value (concept)
-*/
-
-function Thing (label, type, uuid, arcs, value) {
-  this.label = label;
-  this.type = type;
-  this.uuid = uuid;
-  this.arcs = arcs;
-  this.value = value;
-  this.concept = []; //concept array for axioms
-  this.relation = []; //relation array for axioms
-  this.addC = function (item) {
-    this.concept.push(item);
-    return item.uuid;
-  };
-  this.addR = function (item) {
-    this.relation.push(item);
-    return item.uuid;
-  };
-  this.delC = function (uuid) {
-    var i = 0;
-    for(i;i<this.concept.length;i++){
-      if(this.concept[i].uuid === uuid) {
-        this.concept.splice(i,1);
-      }
-    }
-  };
-  this.delR = function (uuid) {
-    var i = 0;
-    for(i;i<this.relation.length;i++){
-      if(this.relation[i].uuid === uuid) {
-        this.relation.splice(i,1);
-      }
-    }
-  };
-  this.find = function (prop, item) {
-    var i = 0;
-    var lC = this.concept.length;
-    for(i;i<lC;i++){
-      if(this.concept[i][prop] === item) {
-        return this.concept[i];
-      }
-    }
-    var y = 0;
-    var lR = this.relation.length;
-    for(y;y<lR;y++){
-      if(this.relation[y][prop] === item) {
-        return this.relation[y];
-      }
-    }
-    return false;
-  };
-}
-
-/* @comment for future development
-So [Person:Jake Butterfield]-(hasView)-[PersonView] where hasView is in fact a relation with function getView(Name, View)
-Assuming @param {graph} View - a view axiom. It can then retrieve data for each field in the view, by first finding Jake Butterfield's Record and then rendering the 'viewGraph' with said values
-Aaaand if no value is passed from link 1 (Person Concept) then it knows to render an empty view
-*/
-
-/*
-* Represents a store of axioms for a domain
-* @constructor
-* @param {string} domainLabel - label of the domain
-*/
-
-function axiomStore (domainLabel) {
- this.label = domainLabel;  // label
- this.axioms = [];  // array to store axioms
- //this.gunPath = gun.get(label); //define base path for db
- this.find = function (label) {
-   var i = 0;
-   var l = this.axioms.length;
-   for(i;i<l;i++){
-     if(this.axioms[i].label === label) {
-       return this.axioms[i];
-     }
-   }
-   console.log('Error: no axiom found');
- };
-}
-
-/*
-* generates uuid and returns an alphanumeric string
-* @returns {string} uuid
-*/
-
-function uuidv4 () {
- return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxx'.replace(/[xy]/g, function(c) {
-   var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-   return v.toString(16);
- })
-}
 
 /*
 * make an instance of a axiomStore and add a few axioms to play with
 */
+
+
+
+
 
 var aStore = new axiomStore ('app');
 
@@ -210,6 +114,14 @@ var date1 = new Thing('Date', 'concept', uuidv4(), {}, '?');
 query.addC(date1);
 var at = new Thing('at', 'relation', uuidv4(), {source:hire1.uuid,target:date1.uuid});
 query.addR(at);
+
+/*
+* Execute test
+*/
+
+var temp = new algoC (query, aStore);
+temp.start()
+
 
 
 /* Utility Functions for Prototype */
