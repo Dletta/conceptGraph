ce865da7412b79efe5f081ba15803754da38c66a
Dletta <jcduschletta@gmail.com>
2019-01-03 21:06:09 -0800

    added deepCompare, added Logic to test out

--- Wumpus/wumpusWorld.js
+++ Wumpus/wumpusWorld.js
@@ -19,43 +19,103 @@ var indy = new Agent(kB, 'Indiana Jones');
 
 var gCounter = 0;
 
-/*
-* A inference system based on closed world
-* and CGs
-* Using Pierces solutions
-*/
-
+/* For inference I need to be able to do exat matching of nested graphs */
+/* Access Graphs from an evaluator or from another context */
 
 /*
-* Logic Or Relation
-* Creates a graph representing an or relation
+* Compare two graphs.
+* @param {graph} graph1 - one to be compared
+* @param {graph} graph2 - two to be compared
+* @returns true if exactly the same, other wise returns false
 */
-function or (graphX, graphY) {
-  var x = graphX; //this should be an axiom
-  var y = graphY; //this should be an axiom
 
-  /* define 3 axioms
-    1 axiom which is [Proposition: GraphX]
-  */
-  var propX = new Thing('Proposition', 'concept', uuidv4(), {}, x);
-  /*
-    2 axiom which is [Proposition: GraphY]
-    */
-  var propY = new Thing('Proposition', 'concept', uuidv4(), {}, y);
-  /*
-    3 axiom wich is (NEG)->[Proposition:
-                      (NEG)->[Proposition: [Proposition: GraphX]]
-                      (NEG)->[Proposition: [Proposition: GraphY]]
-                    ]
-    */
+function deepCompare (graph1, graph2) {
+  let one = graph1;
+  let two = graph2;
+  let result = true;
 
-  var negX = new Thing('Proposition', 'context', uuidv4());
-  var negY = new Thing('Proposition', 'context', uuidv4());
-  negX.addRelation('NEG', 'relation', uuidv4(), {0:negX});
-  negX.addConcept('Proposition', 'concept', uuidv4(), {}, propX);
-  negY.addRelation('NEG', 'relation', uuidv4(), {0:negY});
-  negY.addConcept('Proposition', 'concept', uuidv4(), {}, propY);
-  var or = new Thing('or', 'axiom', uuidv4());
+  for(let item of one.concept) {
+    console.log(item);
+    for(let item2 of two.concept) {
+      console.log(item2);
+      console.log(result);
+      if(item.uuid == item2.uuid && !result) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
 
+  return result;
+}
 
+/* Test inference with below graph showing that if you are born in Oz
+ then you are a citizen. Tinman was born in Oz, therefore it is true he is a citizen.
+ */
+
+var born = new Thing('bornInOz', 'axiom', uuidv4());
+
+var person = born.addConcept('Person', 'concept', uuidv4(), {}, '*');
+var bornC = born.addConcept('Born', 'concept', uuidv4());
+var country = born.addConcept('Country', 'concept', uuidv4(), {}, 'Oz');
+
+born.addRelation('object', 'relation', uuidv4(), {0:bornC,1:person});
+born.addRelation('location', 'relation', uuidv4(), {0:bornC,1:country});
+
+kB.axioms.push(born);
+
+var citizenAxiom = new Thing('citizenOfOz', 'axiom', uuidv4());
+
+var citizen = citizenAxiom.addConcept('Citizen', 'concept', uuidv4(), {}, '*');
+person.coreferent = citizen;
+citizen.coreferent = person;
+var countryCit = citizenAxiom.addConcept('Country', 'concept', uuidv4(), {}, 'Oz');
+
+citizenAxiom.addRelation('member', 'relation', uuidv4(), {0:countryCit,1:citizen});
+
+kB.axioms.push(citizenAxiom);
+
+var negContext2 = new Thing('negativeContext2', 'axiom', uuidv4());
+var prop2 = negContext2.addConcept('Proposition', 'concept', uuidv4(), {}, citizenAxiom);
+negContext2.addRelation('negative', 'relation', uuidv4(), {0:prop2});
+citizenAxiom.parent = negContext2;
+
+kB.axioms.push(negContext2);
+
+var negContext1 = new Thing('negativeContext1', 'axiom', uuidv4());
+var prop1 = negContext1.addConcept('Proposition', 'concept', uuidv4(), {}, born);
+negContext1.addRelation('negative', 'relation', uuidv4(), {0:prop1})
+born.parent = negContext1;
+
+kB.axioms.push(negContext1);
+
+var tinmanA = new Thing('tinmanBorninOz', 'axiom', uuidv4());
+
+var tinman = tinmanA.addConcept('Person', 'concept', uuidv4(), {}, 'Tinman');
+var bornTin = tinmanA.addConcept('Born', 'concept', uuidv4());
+var countryTin = tinmanA.addConcept('Country', 'concept', uuidv4(), {}, 'Oz');
+tinmanA.addRelation('object', 'relation', uuidv4(), {0:bornTin,1:tinman});
+tinmanA.addRelation('location', 'relation', uuidv4(), {0:bornTin,1:countryTin});
+
+function translate(graph) {
+  var string = '';
+  var i = 0;
+  var l = graph.relation.length;
+  for(i;i<l;i++){
+    let temp = graph.relation[i];
+    console.log(temp);
+    string += graph.find('uuid',temp.arcs[0]).label;
+    string += ':';
+    string += graph.find('uuid',temp.arcs[0]).value;
+    string += ' ';
+    string += graph.relation[i].label;
+    string += ' ';
+    string += graph.find('uuid', temp.arcs[1]).label;
+    string += ':';
+    string += graph.find('uuid', temp.arcs[1]).value;
+    string += ' ';
+    string += '::; '
+  }
+  console.log(string);
 }
