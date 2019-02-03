c94205ae9edc591902e4292efbc41f7208c6d2cc
Dletta <jcduschletta@gmail.com>
2019-02-03 14:34:36 -0800

    added comments and some tests

--- Wumpus/wumpusWorld.js
+++ Wumpus/wumpusWorld.js
@@ -177,70 +177,18 @@ var no2Owners = parse('No two Owners::[Owner:**x](agent?x?y)[Buy:**y](agent?z?y)
 neg.add(no2Owners);
 
 
-console.log('PUZZLE ///////////////////////////////////////////////////////////////////////');
-
-var init1 = parse('Init One Joan::[Owner:Joan*x](agent?x?y)[Own:1*y](object?y?z)[Bird:**z](object?c?z)[Buy:1*c](pTime?c?a)[Month:**a](location?c?b)[City:**b]');
-canon.add(init1);
-var init2 = parse('Init Two Ida::[Owner:Ida*d](agent?d?e)[Own:2*e](object?e?f)[Bird:**f](object?g?f)[Buy:2*g](pTime?g?h)[Month:**h](location?g?i)[City:**i]');
-canon.add(init2);
-var init3 = parse('Init Three Pam::[Owner:Pam*j](agent?j?k)[Own:3*k](object?k?l)[Bird:**l](object?m?l)[Buy:3*m](pTime?m?n)[Month:**n](location?m?o)[City:**o]');
-canon.add(init3);
-var init4 = parse('Init Four Faye::[Owner:Faye*p](agent?p?q)[Own:4*q](object?q?r)[Bird:**r](object?s?r)[Buy:4*s](pTime?s?t)[Month:**t](location?s?u)[City:**u]');
-canon.add(init4);
-var init5 = parse('Init Five Celia::[Owner:Celia*v](agent?v?w)[Own:5*w](object?w?X)[Bird:**X](object?Y?X)[Buy:5*Y](pTime?Y?Z)[Month:**Z](location?Y?A)[City:**A]');
-canon.add(init5);
-
-
-console.log('first');
-var first = parse('First Clue::[Bird:Lorikeet*x](object?y?x)[Buy:**y](location?y?z)[City:Berkley*z]');
-puzzle.init(first, canon, law, neg);
-
-
-console.log('first');
-var first = parse('First Clue::[Bird:Lorikeet*x](object?y?x)[Buy:**y](location?y?z)[City:Berkley*z]');
-//puzzle.tell(first);
-console.log(puzzle.states);
-
-console.log('second');
-var second = parse('Second Clue::[Bird:Canary*x](object?y?x)[Buy:**y](pTime?y?z)[Month:**z](Before?z?a)[Month:**a](pTime?b?a)[Buy:**b](location?b?c)[City:PanoramaCity*c]');
-//puzzle.tell(second);
-
-console.log('thirdOne');
-var thirdOne = parse('Third One Option::[Bird:Parrot*x](object?y?x)[Own:**y](agent?z?y)[Owner:Celia*z](object?a?x)[Buy:**a](pTime?a?b)[Month:March*b]');
-//puzzle.tell(thirdOne);
-
-console.log('thirdTwo');
-var thirdTwo = parse('Third One Option::[Bird:Parrot*x](object?y?x)[Own:**y](agent?z?y)[Owner:Celia*z](object?a?x)[Buy:**a](location?a?b)[City:Inyokern*b]');
-//puzzle.tell(thirdTwo);
-
-console.log('fourth');
-var fourth = parse('Fourth Clue::[Owner:Ida*x](agent?x?y)[Own:**y](object?y?z)[Bird:Lovebird*z](object?d?z)[Buy:**d](pTime?d?a)[Month:February*a]');
-//puzzle.tell(fourth);
-
-console.log('fifth');
-var fifth = parse('Fifth Clue::[Bird:Parrot*x](object?y?x)[Buy:4*y]');
-//puzzle.tell(fifth);
-
-console.log('sixth');
-var sixth = parse('Sixth Clue::[Owner:Pam*x](agent?x?y)[Own:2*y](object?y?z)[Bird:**z](object?a?z)[Buy:**a](pTime?a?b)[Month:**b](after?b?c)[Month:**c](pTime?d?c)[Buy:**d](object?d?e)[Bird:Lovebird*e]');
-//puzzle.tell(sixth);
-
-console.log('seven');
-var seven = parse('Seventh Clue::[Bird:**x](object?y?x)[Buy:**y](location?y?z)[City:RollingHills*z](pTime?y?a)[Month:January*a]');
-//puzzle.tell(seven);
-
-console.log('eigth');
-// */
-
 
 var testWorld = new worldBasis();
 
 var testCanon = new axiomStore('test Canon');
 
-var testSchema = parse(' Test Person ::[Person:**x](has?x?y)[Name:**y]');
+var testSchema = parse('Test Person ::[Person:**x](has?x?y)[Name:**y]');
 testCanon.add(testSchema);
 
 var perJohn = parse('John ::[Person:John*x]');
 
 
 testWorld.init(perJohn, testCanon, law, neg);
+testWorld.tell(perJohn);
+
+testWorld.tell(parse('JohnsName is John::[Person:John*x](has?x?y)[Name:John*y]')); //will not work, as it does not 'add new Information' currently
